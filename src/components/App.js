559bcf7cf8e90f001dc974fa286d40f5955d1a20
mercuri <mercuri90028@gmail.com>
2022-09-06 19:56:37 +0530

    try this one

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
+  
   const handleClick = (event) =>{
     // use console.log
-    console.log(event.currentTarget.id);
+    event.preventDefault()
+    console.log(`Button id is:-${event.target.id}`);
+    
+    
   }
 
   // do not remove the two buttons or change their id
@@ -12,6 +16,7 @@ const App = () => {
     <div id="main">
       <button id="button-a" onClick={handleClick}>Button A</button>
       <button id="button-b" onClick={handleClick}>Button B</button>
+      
     </div>
   )
 }
